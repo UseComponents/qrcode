0a9cd98801b63b8ad5e6df3edbaf62fa9eee81e1
ZhouWei <1244620067@qq.com>
2024-01-04 16:02:39 +0800

    chore: support vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
       entryRoot: 'src',
       outDir: ['es', 'lib'],
     }),
+    VueDevTools()
   ],
   resolve: {
     alias: {
