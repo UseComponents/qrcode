c724da664e33e174ff7e0c3120f30c547247aa9b
ZhouWei <1244620067@qq.com>
2024-01-06 15:34:21 +0800

    docs: update demo

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,155 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import QRCode, { QRCodeCanvas, QRCodeSVG } from './components'
+import { ref, computed } from 'vue'
+import QRCode from './components'
 import type { QRCodeProps } from './components'
-
+import { version } from '../package.json'
 const size = ref(160)
-const level = ref<QRCodeProps['errorLevel']>('L')
-const qrcodeCanvasRef = ref()
-const decline = () => {
-  size.value = size.value - 10
-  if (size.value < 48) {
-    size.value = 48
+const bgColor = ref('#ffffff')
+const color = ref('#000000')
+const level = ref('L')
+const value = ref('https://vuejs.org')
+const type = ref<QRCodeProps['type']>('canvas')
+const includeImage = ref(false)
+const icon = ref('https://vuejs.org/images/logo.png')
+const iconSize = ref(40)
+const status = ref<QRCodeProps['status']>('active')
+const bordered = ref(true)
+const centerImage = ref(true)
+const imageX = ref(0)
+const imageY = ref(0)
+const excavate = ref(false)
+const outputCode = computed(() => {
+  const iconSettings = {
+    x: imageX.value,
+    y: imageY.value,
+    excavate: excavate.value
   }
-}
-const increase = () => {
-  size.value = size.value + 10
-  if (size.value > 300) {
-    size.value = 300
-  }
-}
-const selectChange = (e: any) => {
-  level.value = e.target.value
-}
-const downloadChange = async () => {
-  try {
-    const url = await qrcodeCanvasRef.value.toDataURL()
-    console.log(url, qrcodeCanvasRef.value.toDataURL)
-    const a = document.createElement('a')
-    a.download = 'QRCode.png'
-    a.href = url
-    document.body.appendChild(a)
-    a.click()
-    document.body.removeChild(a)
-  } catch (error) {
-    console.log(error)
-  }
-}
-const qrcodeProps = {
-  value: 'https://vuejs.org',
-  size: 134,
-  level: 'M',
-  bgColor: 'transparent',
-  fgColor: 'rgba(0, 0, 0, 0.88)',
-  imageSettings: {
-    src: 'https://vuejs.org/images/logo.png',
-    x: undefined,
-    y: undefined,
-    height: 40,
-    width: 40,
-    excavate: true
-  }
-}
+  return `
+import QRCode from "qrcode.vue-next";
+
+<template>
+  <QRCode
+    :value="${value.value}"
+    :size="${size.value}"
+    :bgColor="${bgColor.value}"
+    :color="${color.value}"
+    :level="${level.value}"
+    :type="${type.value}"
+    :icon="${includeImage.value ? icon.value : undefined}"
+    :iconSize="${includeImage.value ? iconSize.value : undefined}"
+    :iconSettings="${centerImage.value ? undefined : JSON.stringify(iconSettings)}"
+    :status="${status.value}"
+    :bordered="${bordered.value}"
+  />
+</template>
+`
+})
 </script>
 
 <template>
-  <QRCode value="https://vuejs.org" bg-color="#cccccc" />
-  <QRCode value="https://vuejs.org" color="#cccccc" />
-  <hr />
-  <QRCode value="https://vuejs.org" icon="https://vuejs.org/images/logo.png" />
-  <hr />
-  <QRCode value="https://vuejs.org" type="svg" status="loading">
-    <template #status>加载中...</template>
-  </QRCode>
-  <QRCode value="https://vuejs.org" type="svg" status="expired" />
-  <QRCode value="https://vuejs.org" type="svg" status="scanned" />
-  <hr />
-  <button @click="decline">- small</button>
-  <button @click="increase">+ large</button>
-  <br />
-  <QRCode
-    value="https://vuejs.org"
-    type="svg"
-    icon="https://vuejs.org/images/logo.png"
-    :size="size"
-    :iconSize="size / 4"
-    error-level="Q"
-  />
-  <hr />
-  <select name="select" @change="selectChange">
-    <option value="L">L</option>
-    <option value="M">M</option>
-    <option value="Q">Q</option>
-    <option value="H">H</option>
-  </select>
-  <QRCode
-    value="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-    :error-level="level"
-  />
-  <hr />
-  <QRCode value="https://vuejs.org" ref="qrcodeCanvasRef" />
-  <button @click="downloadChange">下载</button>
-  <hr />
-  <QRCodeCanvas v-bind="qrcodeProps" />
-  <br />
-  <QRCodeSVG v-bind="qrcodeProps" />
+  <h1>
+    QRCode.vue-next Demo - v<a
+      :href="`https://www.npmjs.com/package/qrcode.vue-next/v/${version}`"
+      target="_blank"
+      >{{ version }}</a
+    >
+  </h1>
+  <div class="content">
+    <fieldset>
+      <legend>Basic Settings</legend>
+      <label>二维码渲染大小-Size(px): <input type="number" v-model="size" /></label>
+      <br />
+      <label>二维码背景颜色-Background Color: <input type="color" v-model="bgColor" /></label>
+      <br />
+      <label>二维码颜色-Color: <input type="color" v-model="color" /></label>
+      <br />
+      <label
+        >二维码纠错等级-Error Level:
+        <select v-model="level">
+          <option value="L">L</option>
+          <option value="M">M</option>
+          <option value="Q">Q</option>
+          <option value="H">H</option>
+        </select>
+      </label>
+      <br />
+      <br />
+      <label>扫描后的地址-Value: <textarea v-model="value" /></label>
+      <br />
+      <br />
+      <label
+        >渲染类型-Type:
+        <select v-model="type">
+          <option value="canvas">Canvas</option>
+          <option value="svg">SVG</option>
+        </select>
+      </label>
+      <br />
+      <label
+        >二维码状态-Status:
+        <select v-model="status">
+          <option value="active">Active</option>
+          <option value="expired">Expired</option>
+          <option value="loading">Loading</option>
+          <option value="scanned">Scanned</option>
+        </select>
+      </label>
+      <br />
+      <label>二维码边框-Bordered: <input type="checkbox" v-model="bordered" /></label>
+      <br />
+      <label>是否使用logo: <input type="checkbox" v-model="includeImage" /></label>
+      <fieldset :disabled="!includeImage">
+        <legend>Icon Settings</legend>
+        <label>Icon: <textarea v-model="icon" /></label>
+        <br />
+        <label>Icon Size {{ iconSize }}: <input type="number" v-model="iconSize" /></label>
+        <br />
+        <label>logo是否居中: <input type="checkbox" v-model="centerImage" /></label>
+        <br />
+        <fieldset :disabled="centerImage">
+          <legend>Image Seeings</legend>
+          <label
+            >Image X {{ imageX }}:
+            <input type="range" min="0" :max="size - (12 + 1) * 2 - iconSize" v-model="imageX"
+          /></label>
+          <br />
+          <label
+            >Image Y {{ imageY }}:
+            <input type="range" min="0" :max="size - (12 + 1) * 2 - iconSize" v-model="imageY"
+          /></label>
+        </fieldset>
+        <br />
+        <label>图标是否有背景: <input type="checkbox" v-model="excavate" /></label>
+      </fieldset>
+    </fieldset>
+    <div>
+      <fieldset>
+        <legend>Output Code</legend>
+        <textarea cols="80" :rows="outputCode.split('\n').length" :value="outputCode" readonly />
+        <QRCode
+          :value="value"
+          :size="size"
+          :level="level"
+          :bg-color="bgColor"
+          :color="color"
+          :type="type"
+          :icon="includeImage ? icon : undefined"
+          :icon-size="includeImage ? iconSize : undefined"
+          :status="status"
+          :bordered="bordered"
+          :icon-settings="{
+            x: centerImage ? undefined : imageX,
+            y: centerImage ? undefined : imageY,
+            excavate: excavate
+          }"
+        />
+      </fieldset>
+    </div>
+  </div>
 </template>
+
+<style>
+.content {
+  display: flex;
+}
+</style>
