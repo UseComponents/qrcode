4e31408a5be5a7a2982ed8aab4ce4de9d6c28733
ZhouWei <1244620067@qq.com>
2025-05-26 15:25:07 +0800

    chore: upgrade dependencies

--- vite.build.config.ts
+++ vite.build.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/qrcode/',
+  base: '/qrcode/',
   plugins: [
     vue(),
     vueJsx(),
